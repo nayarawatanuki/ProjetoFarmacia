5fa2f84ecf540a7a6d1c1f5bd30cbc99a472a773
andretharada <andretharada@gmail.com>
2018-08-26 17:32:38 -0300

    teste

---
+++ src/test/testMain.java
@@ -0,0 +1,9 @@
+package test;
+import javax.swing.*;
+
+public class testMain {
+	public static void main(String[] args) {
+		JButton button = new JButton("I'm a Swing button!");
+		button.setMnemonic('i'); 
+	}
+}
