aa4c3a57f7da19f107391a51684ac0cfdc91bf55
andretharada <andretharada@gmail.com>
2018-08-26 17:35:07 -0300

    jfc

--- src/test/testMain.java
+++ src/test/testMain.java
@@ -2,8 +2,29 @@ package test;
 import javax.swing.*;
 
 public class testMain {
+	
+	private static void createAndShowGUI() {
+        //Make sure we have nice window decorations.
+        JFrame.setDefaultLookAndFeelDecorated(true);
+
+        //Create and set up the window.
+        JFrame frame = new JFrame("HelloWorldSwing");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Add the ubiquitous "Hello World" label.
+        JLabel label = new JLabel("Hello World");
+        frame.getContentPane().add(label);
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+	
 	public static void main(String[] args) {
-		JButton button = new JButton("I'm a Swing button!");
-		button.setMnemonic('i'); 
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
 	}
 }
