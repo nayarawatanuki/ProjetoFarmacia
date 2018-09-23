3c648a9df59dcca3748e9076adc269385d3f6af8
nayarawatanuki <nayarawatanuki@192.168.0.116>
2018-09-23 13:41:33 -0300

    VENDA

--- src/br/edu/ifsp/bra/farmacia/Venda.java
+++ src/br/edu/ifsp/bra/farmacia/Venda.java
@@ -18,6 +18,7 @@ import javax.swing.JTextPane;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JMenu;
+import javax.swing.UIManager;
 
 public class Venda {
 
@@ -52,61 +53,70 @@ public class Venda {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 572, 369);
+		frame.setBounds(100, 100, 797, 421);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		table = new JTable();
-		
 		JLabel lblVendas = new JLabel("Vendas");
-		lblVendas.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
-		
-		JLabel lblFConcluir = new JLabel("F2 - CONCLUIR   F3 - APAGAR   F4 - CANCELAR VENDA   F5 - DESCONTO");
-		lblFConcluir.setFont(new Font("Lucida Grande", Font.PLAIN, 10));
+		lblVendas.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
 		
 		JTextPane txtpnFarmciaTel = new JTextPane();
 		txtpnFarmciaTel.setText("                   \n\n            FARMÁCIA   \n\n       Tel.: 3468-2211");
+		
+		JTextPane textPane = new JTextPane();
+		textPane.setText("TOTAL:");
+		textPane.setFont(new Font("Lucida Grande", Font.PLAIN, 13));
+		textPane.setEnabled(false);
+		textPane.setEditable(false);
+		
+		JTextPane textPane_1 = new JTextPane();
+		textPane_1.setText("SUBTOTAL: ");
+		textPane_1.setEnabled(false);
+		textPane_1.setEditable(false);
+		
+		table = new JTable();
+		
+		JTextPane txtpnFAdicionar = new JTextPane();
+		txtpnFAdicionar.setForeground(UIManager.getColor("Button.highlight"));
+		txtpnFAdicionar.setFont(new Font("Lucida Grande", Font.PLAIN, 11));
+		txtpnFAdicionar.setText("\n     F2 - ADICIONAR   F3 - QUANTIDADE   F4 - CONSULTA   F5 - PAGAMENTO   F6 - DESCONTO   F7 - OPÇÕES   F8 - CANCELAR");
+		txtpnFAdicionar.setBackground(UIManager.getColor("Focus.color"));
 		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
 		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup(Alignment.TRAILING)
+			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(68)
-					.addComponent(lblFConcluir)
-					.addContainerGap(155, Short.MAX_VALUE))
-				.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
 					.addGap(45)
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addComponent(table, GroupLayout.PREFERRED_SIZE, 290, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblVendas, GroupLayout.PREFERRED_SIZE, 69, GroupLayout.PREFERRED_SIZE))
-					.addPreferredGap(ComponentPlacement.RELATED, 54, Short.MAX_VALUE)
-					.addComponent(txtpnFarmciaTel, GroupLayout.PREFERRED_SIZE, 158, GroupLayout.PREFERRED_SIZE)
-					.addGap(25))
+					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING, false)
+						.addComponent(txtpnFAdicionar, Alignment.LEADING)
+						.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
+								.addComponent(table, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+								.addGroup(groupLayout.createSequentialGroup()
+									.addComponent(textPane, GroupLayout.PREFERRED_SIZE, 147, GroupLayout.PREFERRED_SIZE)
+									.addPreferredGap(ComponentPlacement.UNRELATED)
+									.addComponent(textPane_1, GroupLayout.PREFERRED_SIZE, 151, GroupLayout.PREFERRED_SIZE))
+								.addComponent(lblVendas))
+							.addGap(246)
+							.addComponent(txtpnFarmciaTel, GroupLayout.PREFERRED_SIZE, 167, GroupLayout.PREFERRED_SIZE)))
+					.addContainerGap(29, Short.MAX_VALUE))
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(32)
+					.addGap(22)
 					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 						.addGroup(groupLayout.createSequentialGroup()
 							.addComponent(lblVendas, GroupLayout.PREFERRED_SIZE, 27, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addComponent(table, GroupLayout.PREFERRED_SIZE, 165, GroupLayout.PREFERRED_SIZE)
-							.addGap(41))
-						.addGroup(groupLayout.createSequentialGroup()
-							.addComponent(txtpnFarmciaTel, GroupLayout.PREFERRED_SIZE, 143, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.RELATED)))
-					.addComponent(lblFConcluir)
-					.addGap(22))
+							.addGap(28)
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addComponent(textPane_1, GroupLayout.PREFERRED_SIZE, 56, GroupLayout.PREFERRED_SIZE)
+								.addComponent(textPane, GroupLayout.PREFERRED_SIZE, 56, GroupLayout.PREFERRED_SIZE))
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addComponent(table, GroupLayout.PREFERRED_SIZE, 125, GroupLayout.PREFERRED_SIZE))
+						.addComponent(txtpnFarmciaTel, GroupLayout.PREFERRED_SIZE, 139, GroupLayout.PREFERRED_SIZE))
+					.addGap(60)
+					.addComponent(txtpnFAdicionar, GroupLayout.PREFERRED_SIZE, 50, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(19, Short.MAX_VALUE))
 		);
 		frame.getContentPane().setLayout(groupLayout);
-		
-		JMenuBar menuBar = new JMenuBar();
-		menuBar.setToolTipText("");
-		frame.setJMenuBar(menuBar);
-		
-		JMenu mnCadastro = new JMenu("Cadastro");
-		menuBar.add(mnCadastro);
-		
-		JMenu mnConsulta = new JMenu("Consulta");
-		menuBar.add(mnConsulta);
 	}
 }
