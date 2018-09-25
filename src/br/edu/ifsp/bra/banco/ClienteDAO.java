cc373ad76c4bfa96809a08ecd8457a1bb58af389
andretharada <andretharada@gmail.com>
2018-09-24 23:46:08 -0300

    Atualizando ClienteDAO.java

--- src/br/edu/ifsp/bra/banco/ClienteDAO.java
+++ src/br/edu/ifsp/bra/banco/ClienteDAO.java
@@ -16,9 +16,9 @@ public class ClienteDAO {
 		try {
 			Statement stmt = connection.createStatement();
 			ResultSet rs = stmt.executeQuery(
-					"SELECT cliente_id, nome, cpf, data_nascimento, data_cadastro FROM cliente " + 
-							"JOIN pessoa on cliente.cliente_id = pessoa.pessoa_id " + 
-							"WHERE cliente_id=" + id);
+					"SELECT cliente_id, nome, endereco, telefone, cpf, data_nascimento, is_ativo, data_cadastro FROM pessoa p " + 
+							"JOIN cliente c on c.cliente_id = p.pessoa_id " + 
+							"WHERE c.cliente_id=" + id);
 
 			if(rs.next()) {
 				return toCliente(rs);
@@ -33,8 +33,8 @@ public class ClienteDAO {
 		Connection connection = ConnectionFactory.getConnection();
 		try {
 			Statement stmt = connection.createStatement();
-			ResultSet rs = stmt.executeQuery("SELECT * FROM cliente " +
-					"JOIN pessoa on cliente.cliente_id = pessoa.pessoa_id");
+			ResultSet rs = stmt.executeQuery("SELECT * FROM pessoa p" +
+					"JOIN cliente c on c.cliente_id = p.pessoa_id");
 			Set<Cliente> clientes = new HashSet<Cliente>();
 			while(rs.next()) {
 				Cliente cliente = toCliente(rs);
@@ -50,9 +50,10 @@ public class ClienteDAO {
 	public boolean novoCliente(Cliente cliente) {
 		Connection connection = ConnectionFactory.getConnection();
 		try {
-			PreparedStatement ps = connection.prepareStatement("INSERT INTO cliente VALUES (?, ?)");
+			PreparedStatement ps = connection.prepareStatement("INSERT INTO cliente VALUES (?, ?, ?)");
 			ps.setInt(1, cliente.getId());
-			ps.setDate(2, cliente.getDataCadastro());
+			ps.setBoolean(2, cliente.isAtivo());
+			ps.setDate(3, cliente.getDataCadastro());
 
 			if (ps.executeUpdate() == 1) {
 				return true;
@@ -67,12 +68,13 @@ public class ClienteDAO {
 	public boolean modificaCliente(Cliente cliente) {
 		Connection connection = ConnectionFactory.getConnection();
 		try {
-			PreparedStatement ps = connection.prepareStatement("UPDATE cliente AS c "
-				+ "JOIN pessoa AS p ON p.pessoa_id=c.cliente_id "
-				+ "SET c.data_cadastro=?, p.nome=? WHERE c.cliente_id=?");
+			PreparedStatement ps = connection.prepareStatement("UPDATE cliente c "
+				+ "JOIN pessoa p ON p.pessoa_id = c.cliente_id "
+				+ "SET c.is_ativo=?, c.data_cadastro=?, p.nome=? WHERE c.cliente_id=?");
+			ps.setBoolean(1, cliente.isAtivo());
 			ps.setDate(1, cliente.getDataCadastro());
-			ps.setString(2, cliente.getNome());
-			ps.setInt(3, cliente.getId());
+			ps.setString(3, cliente.getNome());
+			ps.setInt(4, cliente.getId());
 
 			if (ps.executeUpdate() == 1) {
 				return true;
@@ -88,9 +90,12 @@ public class ClienteDAO {
 		Cliente cliente = new Cliente();
 		cliente.setId(rs.getInt("cliente_id"));
 		cliente.setNome(rs.getString("nome"));
+		cliente.setEndereco(rs.getString("endereco"));
+		cliente.setTelefone(rs.getString("telefone"));
 		cliente.setCpf(rs.getString("cpf"));
 		cliente.setDataNascimento(rs.getDate("data_nascimento"));
 		cliente.setDataCadastro(rs.getDate("data_cadastro"));
+		cliente.setAtivo(rs.getBoolean("is_ativo"));
 		return cliente;
 	}
 }
