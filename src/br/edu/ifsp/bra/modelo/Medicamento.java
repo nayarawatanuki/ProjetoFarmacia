b60204f9e525d04e69a4fb3f6955436ea698fbbc
andretharada <andretharada@gmail.com>
2018-09-24 22:19:00 -0300

    Corrigindo o domínio

--- src/br/edu/ifsp/bra/modelo/Medicamento.java
+++ src/br/edu/ifsp/bra/modelo/Medicamento.java
@@ -11,7 +11,7 @@ public class Medicamento {
 	private TipoMedicamento tipo;
 	
 	public Medicamento() {};
-	public Medicamento(String codigo, String descricao, double preco, int estoque, TipoMedicamento tipo, double desconto) {
+	public Medicamento(String codigo, String descricao, double preco, int estoque, TipoMedicamento tipo) {
 		this.codigo = codigo;
 		this.descricao = descricao;
 		this.preco = preco;
