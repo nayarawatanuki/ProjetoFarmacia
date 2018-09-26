f17792633059a6317dfbf384dd2d715ff6b80fca
andretharada <andretharada@gmail.com>
2018-09-26 00:59:34 -0300

    Adicionando funcionalidades no caixa

--- src/br/edu/ifsp/bra/dominio/CaixaBLL.java
+++ src/br/edu/ifsp/bra/dominio/CaixaBLL.java
@@ -1,55 +1,98 @@
 package br.edu.ifsp.bra.dominio;
 
-import java.time.LocalDateTime;
-import java.util.Date;
+import java.sql.Date;
+import java.util.Calendar;
 
 import br.edu.ifsp.bra.banco.PedidoDAO;
 import br.edu.ifsp.bra.modelo.Funcionario;
+import br.edu.ifsp.bra.modelo.ItemPedido;
+import br.edu.ifsp.bra.modelo.Medicamento;
 import br.edu.ifsp.bra.modelo.Pedido;
-import br.edu.ifsp.bra.modelo.Pedido.statusPedido;
+import br.edu.ifsp.bra.modelo.Pedido.StatusPedido;
 
 public class CaixaBLL {
-	
-	static Pedido pedidoAtual;
-	static boolean CaixaAberto;
-	static Funcionario funcionario;
-	static int numeroPedido;
-	
+
+	private static PedidoDAO pedidoDAO = new PedidoDAO();
+
+	private static Pedido pedidoAtual;
+	private static Funcionario funcionarioAtual;
+	private static boolean isAberto;
+
 	public static Pedido getPedidoAtual() {
 		return pedidoAtual;
 	}
 
-	public static boolean isCaixaAberto() {
-		return CaixaAberto;
+	public static Funcionario getFuncionarioAtual() {
+		return funcionarioAtual;
 	}
 
-	public static Funcionario getFuncionario() {
-		return funcionario;
-	}
-
-	public static void setFuncionario(Funcionario funcionario) {
-		CaixaBLL.funcionario = funcionario;
+	public static boolean isAberto() {
+		return isAberto;
 	}
 
 	public void abrirCaixa(Funcionario f) {
-		
+		if (CaixaBLL.isAberto() && CaixaBLL.getFuncionarioAtual() != null) {
+			throw new RuntimeException("O caixa não pode ser aberto pois já está aberto"); // CaixaAbertoException
+		}
+
+		CaixaBLL.funcionarioAtual = f;
+		CaixaBLL.isAberto = true;
 	}
-	
+
 	public void fecharCaixa(Funcionario f) {
+		if (CaixaBLL.getPedidoAtual() != null) {
+			throw new RuntimeException("O caixa não pode ser fechado pois um pedido está aberto"); // PedidoAbertoException
+		}
+
+		CaixaBLL.funcionarioAtual = null;
+		CaixaBLL.isAberto = false;
+	}
+
+	public void adicionaMedicamento(Medicamento m, int quantidade) {
+		CaixaBLL.getPedidoAtual().adicionaItem(new ItemPedido(CaixaBLL.getPedidoAtual().getId(), m.getId(), m.getPreco(), quantidade));
+	}
+
+	public void novoPedido() {
+		if (CaixaBLL.getPedidoAtual() != null) {
+			throw new RuntimeException("Um pedido ainda está em aberto"); // PedidoAbertoException
+		}
+
+		CaixaBLL.pedidoAtual = new Pedido();
+		CaixaBLL.getPedidoAtual().setCaixaId(1);
+		CaixaBLL.getPedidoAtual().setData(new Date(Calendar.getInstance().getTime().getTime()));
+		CaixaBLL.getPedidoAtual().setStatus(StatusPedido.ABERTO);
 		
+		// Retornando o último id
+		// CaixaBLL.getPedidoAtual().setId(pedidoDAO.adicionar(CaixaBLL.getPedidoAtual()));
+	}
+
+	public void efetuaVenda() {
+		this.alteraStatus(StatusPedido.FECHADO);
+	}
+
+	public void cancelaVenda() {
+		if (CaixaBLL.getPedidoAtual() == null) {
+			throw new RuntimeException("Não existe nenhuma pedido em aberto para cancelar"); // PedidoInvalidoException
+		}
+
+		this.alteraStatus(StatusPedido.CANCELADO);
+		CaixaBLL.pedidoAtual = null;
 	}
 	
-	public void novoPedido() {
-		if(pedidoAtual.getStatus() != statusPedido.ABERTO) {
-			
-			//pedidoAtual = PedidoDAO.NovoPedido();
-			
-		}else {
-			//// Acredito que seja bom guardar todo tipo de pedido, desde os cancelados aos abertos que nao foram fechados
-			
-			 // pedidoAtual.setStatus(statusPedido.CANCELADO);
-			//	PedidoDAO.adicionar(pedidoAtual)  Salva o pedido atual
-			// pedidoAtual = PedidoDAO.NovoPedido();	
-		}	
+	public void geraNotaFiscal() {
+
+	}
+	
+	private void alteraStatus(StatusPedido status) {
+		if (CaixaBLL.getPedidoAtual() != null) {
+			throw new RuntimeException("Um pedido ainda está em aberto"); // PedidoAbertoException
+		}
+		if (CaixaBLL.getPedidoAtual().getStatus() == status) {
+			throw new RuntimeException("O status continua sendo o mesmo"); // PedidoStatusException
+		}
+
+		CaixaBLL.getPedidoAtual().setStatus(status);
+		// Modificando o status do pedido
+		// CaixaBLL.pedidoDAO.modificar(CaixaBLL.getPedidoAtual());
 	}
 }
