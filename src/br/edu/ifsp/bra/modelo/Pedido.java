2407d45360dedc816c1b2983b87aa026605ce063
andretharada <andretharada@gmail.com>
2018-09-26 00:27:14 -0300

    Adicionando campos e métodos

--- src/br/edu/ifsp/bra/modelo/Pedido.java
+++ src/br/edu/ifsp/bra/modelo/Pedido.java
@@ -1,83 +1,95 @@
 package br.edu.ifsp.bra.modelo;
 
-import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
 
 public class Pedido {
 
-	public static enum statusPedido implements Serializable {
-		ABERTO, FECHADO, CANCELADO;
-		public String toString() {
-			switch (this) {
-			case ABERTO:
-				return "ABERTO";
-			case FECHADO:
-				return "FECHADO";
-			case CANCELADO:
-				return "CANCELADO";
+	public enum StatusPedido {
+		NENHUM, ABERTO, FECHADO, CANCELADO;
+
+		public StatusPedido getTipo(int tipo) {
+			switch (tipo) {
+			case 1:
+				return ABERTO;
+			case 2:
+				return FECHADO;
+			case 3:
+				return CANCELADO;
+			default:
+				return NENHUM;
 			}
-			return null;
 		}
-	};
 
-	private int numero;
+		public static int setTipo(StatusPedido tipo) {
+			switch (tipo) {
+			case ABERTO:
+				return 1;
+			case FECHADO:
+				return 2;
+			case CANCELADO:
+				return 3;
+			default:
+				return 0;
+			}
+		}
+	}
+
+	private int id;
+	private int caixaId;
+	private StatusPedido status;
 	private List<ItemPedido> itens;
+	private double total;
 	private Date data;
-	private statusPedido status;
 
-	public Pedido() {
-	};
-
-	public Pedido(int numero, List<ItemPedido> itens, Date data, statusPedido status) {
-		super();
-		this.numero = numero;
+	public Pedido() {}
+	public Pedido(int caixaId, List<ItemPedido> itens, Date data, double total, StatusPedido status) {
+		this.caixaId = caixaId;
 		this.itens = itens;
 		this.data = data;
+		this.total = total;
 		this.status = status;
 	}
-
-	public int getNumero() {
-		return numero;
+	public int getId() {
+		return id;
 	}
-
-	public void setNumero(int numero) {
-		this.numero = numero;
+	public void setId(int id) {
+		this.id = id;
 	}
-
+	public int getCaixaId() {
+		return caixaId;
+	}
+	public void setCaixaId(int caixaId) {
+		this.caixaId = caixaId;
+	}
 	public List<ItemPedido> getItens() {
 		return itens;
 	}
-
-	public void setItens(List<ItemPedido> itens) {
-		this.itens = itens;
+	public void adicionaItem(ItemPedido item) {
+		this.itens.add(item);;
 	}
+	public void removeItem(ItemPedido item) {
+		if (!this.itens.contains(item))
+			return;
 
+		this.itens.remove(item);
+	}
+	public StatusPedido getStatus() {
+		return status;
+	}
+	public void setStatus(StatusPedido status) {
+		this.status = status;
+	}
+	public double getTotal() {
+		return total;
+	}
+	public void setTotal(double total) {
+		this.total = total;
+	}
 	public Date getData() {
 		return data;
 	}
-
 	public void setData(Date data) {
 		this.data = data;
-	}
-
-	public statusPedido getStatus() {
-		return status;
-	}
-
-	public void setStatus(statusPedido status) {
-		this.status = status;
-	}
-
-	public void setStatus(String status) {
-		if (status == statusPedido.ABERTO.toString()) {
-			this.status = statusPedido.ABERTO;
-
-		} else if (status == statusPedido.FECHADO.toString()) {
-			this.status = statusPedido.FECHADO;
-
-		} else if (status == statusPedido.CANCELADO.toString()) {
-			this.status = statusPedido.CANCELADO;
-		}
 	}
 }
