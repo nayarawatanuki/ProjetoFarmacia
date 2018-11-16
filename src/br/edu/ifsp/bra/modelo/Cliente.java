a10f08005be9241e5db91b38befe4cd3914e4f2b
andretharada <andretharada@gmail.com>
2018-11-16 16:13:24 -0200

    Removendo isAtivo()

--- src/br/edu/ifsp/bra/modelo/Cliente.java
+++ src/br/edu/ifsp/bra/modelo/Cliente.java
@@ -4,20 +4,12 @@ import java.sql.Date;
 
 public class Cliente extends Pessoa {
 	
-	private boolean isAtivo;
 	private Date dataCadastro;
 	
 	public Cliente() {}
-	public Cliente(String nome, String endereco, String telefone, String cpf, Date dataNascimento, boolean isAtivo, Date dataCadastro) {
+	public Cliente(String nome, String endereco, String telefone, String cpf, Date dataNascimento, Date dataCadastro) {
 		super(nome, endereco, telefone, cpf, dataNascimento);
-		this.isAtivo = isAtivo;
 		this.dataCadastro = dataCadastro;
-	}
-	public boolean isAtivo() {
-		return isAtivo;
-	}
-	public void setAtivo(boolean isAtivo) {
-		this.isAtivo = isAtivo;
 	}
 	public Date getDataCadastro() {
 		return dataCadastro;
